2e5db614c3c49a0c992dd09a70884679a079fb75
mEssiaHofDarK <najent@yandex.ru>
2021-05-30 20:22:26 +0300

    start

---
+++ mail_course/step_1_6/server.py
@@ -0,0 +1,22 @@
+import tornado.web
+import tornado.ioloop
+import asyncio
+
+
+class Handler(tornado.web.RequestHandler):
+
+    async def get(self):
+        utm_source = self.get_argument('edugram_id')
+        print(utm_source)
+        self.write(utm_source)
+        self.flush()
+        self.finish()
+
+
+app = tornado.web.Application([
+    (r'/course/matematika-ege', Handler),
+])
+
+if __name__ == "__main__":
+    app.listen(2222)
+    tornado.ioloop.IOLoop.instance().start()
