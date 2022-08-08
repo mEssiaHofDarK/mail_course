0b6133f40b981c7376cce5ee1214e6549fd1ee5c
m.artem <najent@yandex.ru>
2022-08-08 20:20:45 +0300

    fix config and init

--- mail_course/scripts/init.sh
+++ mail_course/scripts/init.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+echo "creating working tree ..."
 mkdir -p /home/box/web/{public,uploads,etc} /home/box/web/public/{img,css,js}
+echo "working tree created! coping nginx.conf ..."
 cp /home/box/mail_course/mail_course/nginx/nginx.conf /home/box/web/etc
+echo "nginx.conf copied! creating config link ..."
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-if [[ $(systemctl is-enabled nginx) == "enabled" ]]; then
-  service nginx restart
-else
-  service nginx start
-fi
+echo "link created! restarting nginx ..."
+service nginx restart
+echo "init complete!"
+#if [[ $(systemctl is-enabled nginx) == "enabled" ]]; then
+#  service nginx restart
+#else
+#  service nginx start
+#fi
 
 
 
