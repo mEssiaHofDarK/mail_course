13b8fb0842b8430313002c3544996d3da2b212ec
m.artem <najent@yandex.ru>
2022-08-08 20:06:13 +0300

    fix init script

--- mail_course/scripts/init.sh
+++ mail_course/scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /home/box/web/{public,uploads,etc} /home/box/web/public/{img,css,js}
-cp /home/box/mail_cource/nginx/nginx.conf /home/box/web/etc
+cp /home/box/mail_cource/mail_cource/nginx/nginx.conf /home/box/web/etc
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 if [[ $(systemctl is-enabled nginx) == "enabled" ]]; then
   service nginx restart
