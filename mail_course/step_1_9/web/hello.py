f50787e5ef78988ce0a3d9fad2807143ce2263e4
m.artem <najent@yandex.ru>
2022-08-10 20:23:08 +0300

    fix

--- mail_course/step_1_9/web/hello.py
+++ mail_course/step_1_9/web/hello.py
@@ -9,6 +9,6 @@ def app(environ, start_response):
             ('Content-Length', str(len(query)))
         ]
         start_response(status, response_headers)
-        return iter([bytes(query)])
+        return iter([query])
     except KeyError:
         return 1
