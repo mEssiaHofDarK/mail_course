6b764174922bf9d99b89e2f9097d48fdec8e31d3
m.artem <najent@yandex.ru>
2022-08-10 20:36:30 +0300

    fix

--- mail_course/step_1_9/web/hello.py
+++ mail_course/step_1_9/web/hello.py
@@ -2,12 +2,13 @@ def app(environ, start_response):
     """Simplest possible application object"""
     try:
         query = '\n'.join(environ['QUERY_STRING'].split('&'))
+        body = [bytes(i+'\n', 'utf-8') for i in environ['QUERY_STRING'].split('&')]
         # data = b'Hello, World!\n'
         status = '200 OK'
         response_headers = [
             ('Content-type', 'text/plain')
         ]
         start_response(status, response_headers)
-        return iter(bytes(query.encode('ascii')))
+        return body
     except KeyError:
         return 1
