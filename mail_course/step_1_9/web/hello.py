ed07651aa1b42355161bbb313814bfeb8218d26f
m.artem <najent@yandex.ru>
2022-08-10 20:28:20 +0300

    fix

--- mail_course/step_1_9/web/hello.py
+++ mail_course/step_1_9/web/hello.py
@@ -5,10 +5,9 @@ def app(environ, start_response):
         # data = b'Hello, World!\n'
         status = '200 OK'
         response_headers = [
-            ('Content-type', 'text/plain'),
-            ('Content-Length', str(len(query)))
+            ('Content-type', 'text/plain')
         ]
         start_response(status, response_headers)
-        return iter([query])
+        return iter(query)
     except KeyError:
         return 1
