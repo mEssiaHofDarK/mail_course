af9dd4560bb32a77edd8bffe02d66acb3438c4a6
m.artem <najent@yandex.ru>
2022-08-10 10:27:53 +0300

    fix

--- mail_course/step_1_9/web/hello.py
+++ mail_course/step_1_9/web/hello.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
     """Simplest possible application object"""
     try:
-        query = [bytes(arg + '\n') for arg in (environ.get('QUERY_STRING')).split('&')]
+        query = [bytes(arg + '\n') for arg in (environ.get('QUERY_STRING')).encode().split('&')]
         print(query)
         # data = b'Hello, World!\n'
         status = '200 OK'
