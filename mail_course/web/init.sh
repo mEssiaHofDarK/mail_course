deeafd1c9dbfd3294eacb51977976b76b77ce592
m.artem <najent@yandex.ru>
2022-08-27 20:15:53 +0300

    fix init

--- mail_course/web/init.sh
+++ mail_course/web/init.sh
@@ -2,7 +2,7 @@
 
 echo "coping project ..."
 mkdir web
-mv /home/box/mail_course/mail_course/web/* ~/web/*
+mv /home/box/mail_course/mail_course/web/* ~/web/
 echo "coping complete! configure nginx and restart..."
 sudo ln -f /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
