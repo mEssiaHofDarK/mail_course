796f740395725755653b042ec78c5c0ce7ec0926
m.artem <najent@yandex.ru>
2022-08-27 20:18:52 +0300

    fix django config

--- mail_course/web/ask/ask/settings.py
+++ mail_course/web/ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=6_)jq7aasw@y4ux)_i)-d0j$rwbg(-n9)5oio*hgamp)d)!fh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
