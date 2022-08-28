3b2220502c4c49f2963b6d96536e2b3ad179b6dc
m.artem <najent@yandex.ru>
2022-08-28 18:53:58 +0300

    modify project with db

--- mail_course/web/ask/qa/models.py
+++ mail_course/web/ask/qa/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)  # - заголовоквопроса
+    text = models.TextField()  # - полный текст вопроса
+    added_at = models.DateTimeField()  # - дата добавления вопроса
+    rating = models.IntegerField()  # - рейтинг вопроса(число)
+    author = models.ForeignKey(to=User, null=True, on_delete=models.SET_NULL)  # - автор вопроса
+    likes = models.ManyToManyField(to=User)  # - список пользователей, поставивших "лайк"
+    objects = QuestionManager()  # - extended Manager()
+
+    class Meta:
+        db_table = 'questions'
+
+
+class Answer(models.Model):
+    text = models.TextField()  # - текст ответа
+    added_at = models.DateTimeField()  # - дата добавления ответа
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)  # - вопрос, к которому относится ответ
+    author = models.ForeignKey(to=User, null=True, on_delete=models.SET_NULL)  # - автор ответа
+
+    class Meta:
+        db_table = 'answers'
--- mail_course/web/init.sh
+++ mail_course/web/init.sh
@@ -3,9 +3,21 @@
 echo "coping project ..."
 mkdir web
 mv /home/box/mail_course/mail_course/web/* ~/web/
-echo "coping complete! configure nginx and restart..."
+
+echo "start mysql server..."
+sudo /etc/init.d/mysql start
+
+echo "create db 'ask' and django models..."
+mysql -u root -e "create database if not exists ask character set utf8;"
+mysql -u root -e "grant all privileges on ask.* to 'box'@'localhost' with grant option;"
+python3 ./web/aks/manage.py makemigrations
+python3 ./web/aks/manage.py migrate
+
+echo "configure nginx..."
 sudo ln -f /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 echo "run gunicorn ..."
 gunicorn -c /home/box/web/etc/gunicorn_django_config.py ask.wsgi:application &
+
 echo "init complete!"
