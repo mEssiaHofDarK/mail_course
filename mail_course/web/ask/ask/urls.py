cf1be2aca8e6cfe464468881cc0f53e84a662f82
m.artem <najent@yandex.ru>
2022-09-11 20:37:51 +0300

    transfer routs to qa app

--- mail_course/web/ask/ask/urls.py
+++ mail_course/web/ask/ask/urls.py
@@ -13,18 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 # from django.contrib import admin
 # from django.urls import path
-from qa.views import test
 
 urlpatterns = [
     # url(r'admin/', admin.site.urls),
-    url(r'^$', test, name='test'),
-    url(r'^login/', test, name='test'),
-    url(r'^signup/', test, name='test'),
-    url(r'^question/[0-9]+/', test, name='test'),
-    url(r'^ask/', test, name='test'),
-    url(r'^popular/', test, name='test'),
-    url(r'^new/', test, name='test'),
+    url(r'^$', include('qa.urls')),
 ]
